2088836c2adecb2af8e738100d443134a6808c04
Pedro Amaral <pivoamaral@gmail.com>
2022-04-08 01:55:42 -0300

    stable version

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4ec55e7ef99affe49388"; }
+/******/ 		__webpack_require__.h = function() { return "ebfe320f2a95a68b87fa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
